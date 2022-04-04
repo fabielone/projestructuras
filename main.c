d8d7e68970a46a9e398bc0fa6383fda4b18143cc
fabielone <fabielone@gmail.com>
2022-04-04 14:39:19 -0700

    actulizacion

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
-//main
+//Archivo Principal
+
+#include "./menu.h"
+
+
+
+void main(){
+
+    short selecion;
+
+     //para hacer recuadron
+     //funcion para mostra menu y regresar un numero 
+     // selection = menu();
+    
+        while (selecion!=0)
+        {
+            /* code */
+
+
+
+
+
+
+        }
+
+    //funcion de salida 
+
+
+
+
+}
