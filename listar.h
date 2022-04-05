e42fb26f1a26a78d3ada90940552ff9152793a32
fabielone <fabielone@gmail.com>
2022-04-05 16:30:26 -0700

    d

--- listar.h
+++ listar.h
@@ -3,27 +3,20 @@
 
 
 
-struct ArrAlumno listar(struct ArrAlumno *arrDatos){
-       
 
-
-
-    return *arrDatos;
-
-
-
-}
 
 void listar(struct ArrAlumno *arrDatos)
 {
     // system("clear");
 
+   
+
     for (int i = 0; i < 3; i++)
     {
         printf("Estudiande %d \n", i + 1);
-        printf(" \nMatricual: %s", arrDatos[i].Matricula);
-        printf("\nNombre: %s", arrDatos[i].Nombre);
-        printf("\nApellido Paterno: %s", Datos[i].ApellidoP);
+        printf(" \nMatricual: %s", arrDatos->arrDatos1[i].Matricula);
+        printf("\nNombre: %s", arrDatos.arrDatos1[i].Nombre);
+        printf("\nApellido Paterno: %s", arrDatos->arrDatos1[i].ApellidoP);
         printf("\nApellido Materno: %s", Datos[i].ApellidoM);
         printf("\nEdad: %d", Datos[i].Edad);
         printf("\nCarrera: %s\n", Datos[i].Carrera);
